f95e6107dc71620eba22c6f948916600ff69aeb4
Sanan <sanan@MacBook-Pro-Sanan.local>
2023-04-11 16:43:24 +0400

    initial

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+// Regex regexName = new Regex("^m..t$");
+// Regex regexName = new Regex("^[a-oA-O]...n$");
+Regex regexName = new Regex("^[a-oA-O]*");
+
+if (regexName.IsMatch("Orxan"))
+{
+    Console.WriteLine("Uygundu");
+}else{
+ Console.WriteLine("Uygun deyil");
+}
+
+// eli@gmail.com 
+
+// *eli.jpg
